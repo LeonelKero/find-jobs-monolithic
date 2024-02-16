92aef77e7daf1da0ce00880b4c206cdc56c57bc8
LeonelKero <waboleonel@gmail.com>
2024-02-16 02:58:54 -0800

    Initial commit

---
+++ src/test/java/com/wbt/findjobs/FindJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbt.findjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
