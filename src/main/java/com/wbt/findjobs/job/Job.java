4e01be02a5df82bf4a67df75d127c60a893f1cec
LeonelKero <waboleonel@gmail.com>
2024-02-18 01:01:09 -0800

    Entity and repository

--- src/main/java/com/wbt/findjobs/job/Job.java
+++ src/main/java/com/wbt/findjobs/job/Job.java
@@ -1,14 +1,23 @@
 package com.wbt.findjobs.job;
 
-import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private Double minSalary;
     private Double maxSalary;
     private String location;
+
+    public Job() {
+    }
 
     public Job(Long id, String title, String description, Double minSalary, Double maxSalary, String location) {
         this.id = id;
