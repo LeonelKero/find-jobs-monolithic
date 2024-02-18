d4350d38db8bf3fb164c86e8d0e141326dae3ceb
LeonelKero <waboleonel@gmail.com>
2024-02-18 01:15:31 -0800

    Switching to jpa and h2

--- src/main/java/com/wbt/findjobs/job/impl/JobServiceImpl.java
+++ src/main/java/com/wbt/findjobs/job/impl/JobServiceImpl.java
@@ -1,56 +1,56 @@
 package com.wbt.findjobs.job.impl;
 
 import com.wbt.findjobs.job.Job;
+import com.wbt.findjobs.job.JobRepository;
 import com.wbt.findjobs.job.JobService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private final static List<Job> jobs = new ArrayList<>();
+
+    private final JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
 
     @Override
     public List<Job> findAll() {
-        return jobs;
+        return this.jobRepository.findAll();
     }
 
     @Override
     public void create(final Job newJob) {
-        jobs.add(newJob);
+        this.jobRepository.save(newJob);
     }
 
     @Override
     public Job findById(final Long jobId) {
-        final var job = jobs.stream().filter(j -> j.getId().equals(jobId)).findFirst();
-        return job.orElse(null);
+        return this.jobRepository.findById(jobId).orElse(null);
     }
 
     @Override
     public Boolean delete(final Long jobId) {
-        final var jobIterator = jobs.iterator();
-        while (jobIterator.hasNext()) {
-            if (jobIterator.next().getId().equals(jobId)) {
-                jobIterator.remove();
-                return true;
-            }
+        try {
+            this.jobRepository.deleteById(jobId);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
 
     @Override
     public Boolean update(final Long jobId, final Job job) {
-        for (Job j : jobs) {
-            if (j.getId().equals(jobId)) {
-                j.setTitle(job.getTitle());
-                j.setDescription(job.getDescription());
-                j.setLocation(job.getLocation());
-                j.setMaxSalary(job.getMaxSalary());
-                j.setMinSalary(job.getMinSalary());
-                return true;
-            }
+        final var j = this.findById(jobId);
+        if (j != null) {
+            j.setTitle(job.getTitle());
+            j.setDescription(job.getDescription());
+            j.setLocation(job.getLocation());
+            j.setMaxSalary(job.getMaxSalary());
+            j.setMinSalary(job.getMinSalary());
+            return true;
         }
         return false;
     }
